d41d18e23defae5708d3247bb442e374362eb54b
Yurii Kondratenko <yurii.condratenco@gmail.com>
2021-10-20 14:27:07 +0300

    implemented owners and vets lists pages

--- pet-clinic-web/src/main/java/fowox/spring/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/fowox/spring/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package fowox.spring.sfgpetclinic.controllers;
 
+import fowox.spring.sfgpetclinic.sevices.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfVets() {
+    public String listOfVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
