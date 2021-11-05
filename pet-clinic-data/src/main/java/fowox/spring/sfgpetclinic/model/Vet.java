29905f452d5e0a997f1fcaff2e9b60f3ca8f279d
Yurii Kondratenko <yurii.condratenco@gmail.com>
2021-11-05 14:39:04 +0200

    implemented Speciality entity and associated it with Vet

--- pet-clinic-data/src/main/java/fowox/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/fowox/spring/sfgpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package fowox.spring.sfgpetclinic.model;
 
-public class Vet extends Pearson{
+import java.util.Set;
 
+public class Vet extends Pearson{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
